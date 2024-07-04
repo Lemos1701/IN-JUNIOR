44a2b320e45d936260b0d37d5004d652240ac7c6
Vitor Lemos <vt.lemos1701@gmail.com>
2024-07-04 14:54:36 -0300

    feature: nova questão feita

---
+++ calculoDeMedia.js
@@ -0,0 +1,16 @@
+let notas = prompt("Diga as 3 notas separadas por espaço:")
+
+notas = notas.split(" ").map(function(nota){
+    return parseFloat(nota);
+});
+
+let acum = 0;
+for(let i = 0; i < 3; i++){
+    acum += notas[i];
+}
+if(acum/3 >= 6){
+    alert("Aprovado");
+}
+else{
+    alert("Reprovado");
+}
