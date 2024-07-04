2782d3c54f83ba4a101a09c59991fdcec837907c
Vitor Lemos <vt.lemos1701@gmail.com>
2024-07-04 14:59:49 -0300

    refactor: troca de let por var

--- calculoDeMedia.js
+++ calculoDeMedia.js
@@ -1,10 +1,10 @@
-let notas = prompt("Diga as 3 notas separadas por espaço:")
+var notas = prompt("Diga as 3 notas separadas por espaço:")
 
 notas = notas.split(" ").map(function(nota){
     return parseFloat(nota);
 });
 
-let acum = 0;
+var acum = 0;
 for(let i = 0; i < 3; i++){
     acum += notas[i];
 }
